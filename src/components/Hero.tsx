d932b86ca73da659051d690de6b0f64f961351c4
Striking Beast <houshican123@gmail.com>
2026-03-11 09:12:05 +0530

    Remove unused AlertCircle import from Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ArrowRight, Shield, AlertCircle, Award, Code, Download } from 'lucide-react'
+import { ArrowRight, Shield, Award, Code, Download } from 'lucide-react'
 
 export default function Hero() {
   const handleViewWork = () => {
